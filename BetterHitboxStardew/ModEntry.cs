e263c6b924f97f03c19dac2d0e98765e356d8dba
kodfod <fodkod@gmail.com>
2019-12-22 19:45:45 -0500

    init Commit

---
+++ BetterHitboxStardew/ModEntry.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+
+namespace BetterHitboxStardew
+{
+    public class ModEntry : Mod
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += InputEvents_ButtonPressed;
+        }
+
+        private double GetAngle(Vector2 screenPixels)
+        {
+            Vector2 absolutePixels = screenPixels + new Vector2(Game1.viewport.X, Game1.viewport.Y);
+            Vector2 position = new Vector2((int)Game1.player.Position.X + 30, (int)Game1.player.Position.Y);
+            Vector2 delta = absolutePixels - position;
+            double angle = Math.Atan2(-delta.Y, delta.X);
+            double angle2;
+
+            if (angle < 0.0)
+            {
+                angle2 = 2 * Math.PI + angle;
+            }
+            else
+            {
+                angle2 = angle;
+            }
+            angle2 = (angle2 / (2.0 * Math.PI)) * 360;
+            return angle2;
+        }
+
+        private void InputEvents_ButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady || !Context.IsPlayerFree || !(e.Button == SButton.MouseLeft) || Game1.player.isCharging || Game1.player.isRidingHorse() || Game1.player.UsingTool)
+            {
+                return;
+            }
+
+            ICursorPosition cursor = e.Cursor;
+
+            double angle = GetAngle(cursor.ScreenPixels);
+            Vector2 grabTile = Game1.player.getTileLocation();
+
+            angle += 22.5;
+
+            int dir;
+            if (angle < 45)
+            {
+                grabTile += new Vector2(1, 0);
+                dir = Game1.right;
+            }
+            else if (angle < 90)
+            {
+                grabTile += new Vector2(1, -1);
+                dir = Game1.up;
+            }
+            else if (angle < 135)
+            {
+                grabTile += new Vector2(0, -1);
+                dir = Game1.up;
+            }
+            else if (angle < 180)
+            {
+                grabTile += new Vector2(-1, -1);
+                dir = Game1.up;
+            }
+            else if (angle < 225)
+            {
+                grabTile += new Vector2(-1, 0);
+                dir = Game1.left;
+            }
+            else if (angle < 270)
+            {
+                grabTile += new Vector2(-1, 1);
+                dir = Game1.down;
+            }
+            else if (angle < 315)
+            {
+                grabTile += new Vector2(0, 1);
+                dir = Game1.down;
+            }
+            else if (angle < 360)
+            {
+                grabTile += new Vector2(1, 1);
+                dir = Game1.down;
+            }
+            else
+            {
+                grabTile += new Vector2(1, 0);
+                dir = Game1.right;
+            }
+            Game1.player.lastClick = grabTile * 64 + new Vector2(32, 32);
+            Game1.player.FacingDirection = dir;
+        }
+
+    }
+}
